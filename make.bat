6eefc7f36ee2ca63584edd8476117c00d3f424ab
Jakraes <70046168+Jakraes@users.noreply.github.com>
2024-03-05 02:14:26 +0000

    Updated make script

--- make.bat
+++ make.bat
@@ -1,3 +1,14 @@
-Rem gcc ./src/*.c -I./inc/ -s -finline-functions -funroll-loops -o app.exe -lpdcurses
+@ECHO OFF
 
-gcc ./src/*.c -I./inc/ -O3 -s -o app.exe -lpdcurses
+ECHO Compiling...
+gcc -Os -s -fno-ident -fno-asynchronous-unwind-tables ./src/*.c -I./inc -o app.exe -lpdcurses
+ECHO Compiled!
+
+where /q upx.exe 
+IF ERRORLEVEL 1 (
+    ECHO upx.exe doesn't exist in path, please add it to the path if you want to further decrease binary size.
+) ELSE (
+    ECHO Packing...
+    upx.exe -9 app.exe
+    ECHO Packed!
+)
